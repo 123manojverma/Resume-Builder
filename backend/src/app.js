9bb63ae72b1625426ea89d3e0c9382dcd1d84eb0
Manoj <123manojverma2005@gmail.com>
2026-03-18 22:14:05 +0530

    api key integrated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,14 @@
 const express=require("express")
 const cookieParser=require("cookie-parser")
-
+const cors=require("cors")
 const app=express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin:"http://localhost:3001",
+    credentials:true
+})) 
 
 // require all the routes here
 const authRouter=require("./routes/auth.routes")
